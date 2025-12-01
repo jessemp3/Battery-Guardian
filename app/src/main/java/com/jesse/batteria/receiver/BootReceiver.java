cd7019056f6651214fd8d7c3c6c3d2a96373fb95
jesse <nitchsx@gmail.com>
2025-12-01 02:12:26 -0300

    feat: add BootReceiver to start service on device startup

---
+++ app/src/main/java/com/jesse/batteria/receiver/BootReceiver.java
@@ -0,0 +1,29 @@
+package com.jesse.batteria.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import com.jesse.batteria.service.BatteryService;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+
+            Log.d("BootReceiver", "Celular ligado - iniciando BatteryService");
+
+            Intent serviceIntent = new Intent(context, BatteryService.class);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent);
+            } else {
+                context.startService(serviceIntent);
+            }
+        }
+    }
+}
